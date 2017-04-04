764faf3a9b9bd574d602b5c04c4b708c011af3ca
SaltyHash <austin.bowen.314@gmail.com>
2017-04-04 16:21:25 -0400

    Using DBManager now

--- src/info/saltyhash/wormhole/Wormhole.java
+++ src/info/saltyhash/wormhole/Wormhole.java
@@ -1,9 +1,8 @@
 package info.saltyhash.wormhole;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import info.saltyhash.wormhole.persistence.DBManager;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Sound;
@@ -12,77 +11,45 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /** Wormhole plugin. */
 public class Wormhole extends JavaPlugin {
-    private Connection db;
-    
-    @Override
-    public void onDisable() {
-        // Close database
-        try {
-            if (this.db != null) this.db.close();
-        }
-        catch (SQLException e) {
-            // Failed to close database, whatever
-            this.getLogger().warning("Failed to close database; "+e.getLocalizedMessage());
-        }
-        this.getLogger().info("Disabled");
-    }
     
     @Override
     public void onEnable() {
+        // Get the path of the data folder: "/path/to/plugins/Wormhole/"
+        String dataPath = this.getDataFolder().getAbsolutePath() + File.separator;
+        
         // Save default config (doesn't overwrite)
         this.saveDefaultConfig();
         
-        /* Check if we need to rename the DB file from "Wormhole.sqlite.db"
-        /* of v1.3.4, to just "Wormhole.sqlite". */
-        // dataPath = "/path/to/plugins/Wormhole/".
-        String dataPath  = this.getDataFolder().getAbsolutePath()+File.separator;
-        File oldDBFile = new File(dataPath+"Wormhole.sqlite.db");
-        File newDBFile = new File(dataPath+"Wormhole.sqlite");
-        // Old DB file exists and the new one does not?
-        if (oldDBFile.exists() && !newDBFile.exists()) {
-            // Rename the old DB file to the new filename
-            if (oldDBFile.renameTo(newDBFile)) {
-                // Successfully renamed DB file
-                this.getLogger().info("Renamed SQLite database file from "
-                        +"'Wormhole.sqlite.db' to 'Wormhole.sqlite'.");
-            } else {
-                // Failed to rename DB file; abort.
-                this.getLogger().severe(
-                        "Failed to rename SQLite database file!");
-                this.getServer().getPluginManager().disablePlugin(this);
-                return;
-            }
-        }
-        
-        // Connect to database
-        try {
-            Class.forName("org.sqlite.JDBC");
-            this.db = DriverManager.getConnection(
-                    "jdbc:sqlite:"+newDBFile.getAbsolutePath());
-            this.db.createStatement().execute("PRAGMA foreign_keys=ON");
-        }
-        catch (ClassNotFoundException | SQLException e) {
-            // Failed to connect to database; abort.
-            this.getLogger().severe(
-                    "Could not connect to database; "+e.getLocalizedMessage());
-            this.getServer().getPluginManager().disablePlugin(this);
+        // Set up the database
+        DBManager.logger = this.getLogger();
+        DBManager.dbFile = new File(dataPath +"Wormhole.sqlite");
+        if (!DBManager.migrate()) {
+            this.disable();
             return;
         }
-
-        // Set up Economy, Jump, Player, and Sign Managers
-        EconManager   econMgr   = new EconManager(this);
-        JumpManager   jumpMgr   = new JumpManager(this.db, this.getLogger());
-        PlayerManager playerMgr = new PlayerManager(this);
-        SignManager   signMgr   = new SignManager(this.db, jumpMgr, this.getLogger());
+        
+        // Set up Economy
+        EconManager econMgr = new EconManager(this);
         
         // Register event handler and command handler
         this.getServer().getPluginManager().registerEvents(
-            new WormholeEventHandler(this, econMgr, playerMgr, signMgr), this);
+            new WormholeEventHandler(this, econMgr), this);
         WormholeCommandHandler handler =
-            new WormholeCommandHandler(this, econMgr, jumpMgr, playerMgr, signMgr);
+            new WormholeCommandHandler(this, econMgr);
         this.getCommand("wormhole").setExecutor(handler);
-
+        
         this.getLogger().info("Enabled");
+    }
+    
+    @Override
+    public void onDisable() {
+        DBManager.closeConnection();
+        this.getLogger().info("Disabled");
+    }
+    
+    /** Disables the plugin; used in fatal error situations. */
+    private void disable() {
+        this.getServer().getPluginManager().disablePlugin(this);
     }
     
     void playTeleportEffect(Location location) {
@@ -111,4 +78,35 @@ public class Wormhole extends JavaPlugin {
             }
         }
     }
+    
+    /*
+    /** Sets up the database.  Only run during startup.
+     *
+     * @return true / false on success / failure.
+     *
+    private boolean setupDatabase() {
+        // Check if we need to rename the DB file from "Wormhole.sqlite.db"
+        // (versions <=1.3.4), to just "Wormhole.sqlite" (versions >= 1.4.0).
+        // dataPath = "/path/to/plugins/Wormhole/"
+        String dataPath = this.getDataFolder().getAbsolutePath()+File.separator;
+        File oldDBFile  = new File(dataPath+"Wormhole.sqlite.db");
+        File newDBFile  = new File(dataPath+"Wormhole.sqlite");
+        // Old DB file exists and the new one does not?
+        if (oldDBFile.exists() && !newDBFile.exists()) {
+            // Rename the old DB file to the new filename
+            if (oldDBFile.renameTo(newDBFile)) {
+                // Successfully renamed DB file
+                this.getLogger().info("Renamed SQLite database file from "
+                        +"'Wormhole.sqlite.db' to 'Wormhole.sqlite'.");
+            } else {
+                // Failed to rename DB file; abort.
+                this.getLogger().severe(
+                        "Failed to rename SQLite database file!");
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    */
 }
